844ba1bf19a1150e58a8b736e18ed894a299cc21
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-02-03 22:32:47 +0530

    changes the models

---
+++ src/lib/models/playlist/playlist.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+      required: [true, "Who is creating this playlist?"],
+    },
+    category: {
+      type: String,
+      enum: ["watch-later", "channel"],
+      required: [true, "category is required"],
+      lowercase: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: [true, "Playlist name is required"],
+      unique: [true, "name must be unique"],
+    },
+    desc: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: [true, "Playlist description is required"],
+    },
+    videoIds: {
+      type: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: "video",
+          unique: true,
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export const PlaylistModel =
+  mongoose.models["playlist"] || new mongoose.Model("playlist", playlistSchema);
