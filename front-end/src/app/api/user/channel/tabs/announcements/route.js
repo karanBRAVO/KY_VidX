39b54b4ab519320503b561e79f8b4a6dbbf0dd79
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-05-07 19:24:14 +0530

    minor changes

--- front-end/src/app/api/user/channel/tabs/announcements/route.js
+++ front-end/src/app/api/user/channel/tabs/announcements/route.js
@@ -1,4 +1,15 @@
+import { NextResponse } from "next/server";
+
 export const GET = async (req, res) => {
   try {
-  } catch (err) {}
+    return NextResponse.json({
+      success: true,
+      message: "Announcements sent successfully",
+    });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      error: err.message,
+    });
+  }
 };
