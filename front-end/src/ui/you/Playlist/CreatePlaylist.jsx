c01a595f9142a5486aca957662883c1b81e5a708
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-05-07 19:09:11 +0530

    created the playlist components and added them to watch later page along with implementing the backend

---
+++ front-end/src/ui/you/Playlist/CreatePlaylist.jsx
@@ -0,0 +1,139 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { _showNotifier } from "@/lib/_store/features/notifier/notifierSlice";
+
+const CreatePlaylist = ({
+  category = "watch-later" || "channel",
+  open,
+  setOpen,
+}) => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    desc: "",
+    category,
+  });
+  const [loading, setLoading] = useState(false);
+
+  // controlled inputs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "desc" && value.length >= 200) {
+      dispatch(_showNotifier({ msg: "Max Length reached for description." }));
+      return;
+    }
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // submit
+  const handleSubmit = async () => {
+    if (!formData.name || !formData.desc) {
+      dispatch(_showNotifier({ msg: "Please fill all the required fields." }));
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const res = await axios.post(
+        `/api/user/playlist/create-new-playlist`,
+        formData
+      );
+      if (res.data.success) {
+        handleClose();
+      } else {
+        dispatch(_showNotifier({ msg: `${res.data.error}` }));
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // close the box
+  const handleClose = () => {
+    setOpen(false);
+    setFormData((prev) => ({ ...prev, name: "", desc: "" }));
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={"paper"}
+        className="bg-black text-white"
+        sx={{
+          ".MuiDialog-paper": {
+            bgcolor: "#242323",
+            width: "100%",
+          },
+        }}
+      >
+        <DialogTitle className="text-white font-black">
+          Create Playlist
+        </DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText className="text-slate-300 text-base font-medium flex flex-row items-center gap-2">
+            Playlist Name:
+            <input
+              type="text"
+              name="name"
+              id="playlistname"
+              required={true}
+              placeholder="Should be unique"
+              autoComplete="off"
+              value={formData.name}
+              onChange={handleInputChange}
+              className="outline-none px-3 py-2 rounded-none bg-transparent border-[1px] border-solid border-[#000000] border-t-0 border-x-0 text-base font-mono"
+            />
+          </DialogContentText>
+          <DialogContentText className="text-slate-300 text-base font-medium flex flex-row items-center gap-2 mt-2">
+            Playlist Description {`(200)`}:
+            <textarea
+              name="desc"
+              id="playlistdescription"
+              required={true}
+              placeholder="Should be brief"
+              value={formData.desc}
+              onChange={handleInputChange}
+              className="outline-none px-3 py-2 rounded-none bg-transparent border-[1px] border-solid border-[#000000] border-t-0 border-x-0 min-h-[45px] text-base font-mono"
+            ></textarea>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            className="bg-[#242323] text-white text-base font-normal rounded-full"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            className="bg-[#484141] text-white text-base font-normal rounded-full"
+            disabled={loading}
+            onClick={handleSubmit}
+          >
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default CreatePlaylist;
---
+++ front-end/src/ui/you/WatchLater/WatchLater.jsx
@@ -0,0 +1,135 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Container, Typography, Box, Button } from "@mui/material";
+import {
+  CreatePlaylist,
+  NotAuthenticated,
+  PlayListCard,
+  PlayListSkeleton,
+  Wait,
+} from "@/ui/ComponentExporter";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+
+// icons
+import ListIcon from "@mui/icons-material/List";
+
+const WatchLater = () => {
+  const { data: session, status } = useSession();
+
+  const [showCreatePlaylist, setShowCreatePlaylist] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    getPlaylists();
+  }, []);
+
+  // get all playlists
+  const getPlaylists = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.get(
+        `/api/user/playlist/get-all-my-playlists?category=watch-later`
+      );
+      if (res.data.success) {
+        setPlaylists((prev) => res.data.playlistData);
+      } else {
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Container className="min-h-screen mt-[130px]" maxWidth={false}>
+        {status === "loading" ? (
+          <>
+            <div className="w-full flex items-center justify-center">
+              <Wait />
+            </div>
+          </>
+        ) : status === "authenticated" ? (
+          <Box>
+            <div className="flex flex-row items-center gap-2">
+              <ListIcon fontSize="large" className="text-white font-black" />
+              <Typography
+                variant="h4"
+                component={"h1"}
+                className="font-black text-white capitalize m-1 tracking-tight leading-1 flex gap-2 flex-row flex-wrap items-center justify-center"
+              >
+                Watch Later
+                <Typography
+                  variant="caption"
+                  component={"span"}
+                  className="text-slate-800 px-2 py-1 rounded-md bg-gray-400 lowercase"
+                >
+                  now
+                </Typography>
+              </Typography>
+            </div>
+
+            <div>
+              {loading ? (
+                <div>
+                  <PlayListSkeleton totalPlaylists={3} />
+                </div>
+              ) : (
+                <div>
+                  {playlists.length === 0 ? (
+                    <div className="w-full p-1 my-2 flex items-center justify-center flex-wrap flex-col gap-2">
+                      <Typography
+                        variant="h5"
+                        component={"h5"}
+                        className="text-blue-600"
+                      >
+                        No playlist created yet
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        onClick={() => setShowCreatePlaylist(true)}
+                        className="bg-slate-300 text-slate-950 text-base font-normal font-sans capitalize"
+                      >
+                        Create Playlist
+                      </Button>
+                      <CreatePlaylist
+                        category="watch-later"
+                        open={showCreatePlaylist}
+                        setOpen={setShowCreatePlaylist}
+                      />
+                    </div>
+                  ) : (
+                    <div className="w-full flex flex-row items-center justify-evenly flex-wrap">
+                      {playlists.map((playlist, idx) => (
+                        <PlayListCard
+                          key={idx}
+                          name={playlist.name}
+                          imgSrc={playlist.imgSrc}
+                          desc={playlist.desc}
+                        />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          </Box>
+        ) : (
+          <>
+            <div className="w-full flex items-center justify-center">
+              <NotAuthenticated />
+            </div>
+          </>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default WatchLater;
