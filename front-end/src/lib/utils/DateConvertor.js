f8622a4f61e7b80bbc9ff17a5a680eeb5964d0ba
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-05-07 19:02:28 +0530

    convert time in seconds to HH:MM:SS function

--- front-end/src/lib/utils/DateConvertor.js
+++ front-end/src/lib/utils/DateConvertor.js
@@ -4,3 +4,18 @@ export const getLocaleTime = (utcTimeString) => {
   const localTimeString = utcDate.toLocaleString();
   return localTimeString;
 };
+
+// change the time format to HH:MM:SS
+export const getFormatedTime = (seconds) => {
+  let hours = Math.ceil(Math.floor(seconds / 3600));
+  let minutes = Math.ceil(Math.floor((seconds - hours * 3600) / 60));
+  let secs = Math.ceil(seconds - hours * 3600 - minutes * 60);
+
+  return (
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(secs).padStart(2, "0")
+  );
+};
